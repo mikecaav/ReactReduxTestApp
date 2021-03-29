ed03fedc2f3c310ba43078fa0b623d95240e8dcd
Miguel Caballero <caballero.mc98@gmail.com>
2021-03-29 13:34:00 -0600

    Created actions and reducers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import {createStore} from "redux"
+
+
+// ACTIONS
+const increment = () => {
+    return {
+        type: 'INCREMENT',
+    }
+}
+
+const decrement = () => {
+    return {
+        type: 'DECREMENT',
+    }
+}
+
+// REDUCERS
+const counter = (state= 0, action) =>{
+    switch (action.type){
+        case "INCREMENT":
+            return state + 1
+        case "DECREMENT":
+            return state - 1
+        default:
+            return state
+    }
+}
+
+// STORE
+let store = createStore(counter)
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
-reportWebVitals();
+reportWebVitals()
